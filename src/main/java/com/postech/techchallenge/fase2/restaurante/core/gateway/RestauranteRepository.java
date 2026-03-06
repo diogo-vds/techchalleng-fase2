8c7f30345062b72283d6990721888dee1ce9009e
Giulianogoliver <giulianogoliver84@gmail.com>
2026-03-06 00:25:10 -0300

    feat: criação do restaurante dominio e gateway

---
+++ src/main/java/com/postech/techchallenge/fase2/restaurante/core/gateway/RestauranteRepository.java
@@ -0,0 +1,18 @@
+package com.postech.techchallenge.fase2.restaurante.core.gateway;
+
+import com.postech.techchallenge.fase2.restaurante.core.domain.Restaurante;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface RestauranteRepository {
+
+    Restaurante salvar(Restaurante restaurante);
+
+    Optional<Restaurante> buscarPorId(UUID id);
+
+    List<Restaurante> listarTodos();
+
+    void deletar(UUID id);
+}
